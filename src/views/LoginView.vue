cc45e2079705dca752e2628ecc381f52c3a220de
Upqroo <Upqroo@DESKTOP-CC50L0S>
2023-03-14 08:58:29 -0500

    AVANCE 14-03-23

---
+++ src/views/LoginView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import LoginVue from '../components/LoginVue.vue';
+import TheWelcome from '../components/LoginVue.vue'
+</script>
+
+<template>
+  <main>
+    <LoginVue />
+  </main>
+</template>
